1f04813e5174df84d0c181fb14f8b2724090ba5b
Irina <irenserge@mail.ru>
2026-05-06 16:06:16 +0300

    feat: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const closeToOpen = new Map();
+  const openBrackets = new Set();
+  
+  for (const [open, close] of bracketsConfig) {
+    openBrackets.add(open);
+    closeToOpen.set(close, open);
+  }
+  
+  const stack = [];
+  let valid = true;
+  
+  Array.from(str).forEach(char => {
+    if (!valid) return;
+    
+    const isOpen = openBrackets.has(char);
+    const isClose = closeToOpen.has(char);
+    
+    // Только открывающая
+    if (isOpen && !isClose) {
+      stack.push(char);
+    }
+    // Только закрывающая
+    else if (!isOpen && isClose) {
+      if (stack.length === 0 || stack.pop() !== closeToOpen.get(char)) {
+        valid = false;
+      }
+    }
+    // И открывающая, и закрывающая (одинаковые символы)
+    else if (isOpen && isClose) {
+      if (stack.length && stack[stack.length - 1] === char) {
+        stack.pop();
+      } else {
+        stack.push(char);
+      }
+    }
+  });
+  
+  return valid && stack.length === 0;
 };
